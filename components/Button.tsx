b35215f60e56501115729d0d0af2d7b130e4efd7
Roshan <nkroshankumar@gmail.com>
2022-08-26 06:42:52 +0530

    button comp created

---
+++ components/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+
+const Button = ({children,link}:any) => {
+  return (
+    <Link href={link?`/${link}`:'/'}>
+    <motion.button
+    className='px-6 py-2 font-rale bg-gradient-to-r from-emerald-600 to-teal-700 text-white rounded-lg block mx-auto my-4'
+    whileHover={{scale:1.15}}
+    whileTap={{scale:0.8}}
+    >
+        {children}
+    </motion.button>
+    </Link>
+  )
+}
+
+export default Button
