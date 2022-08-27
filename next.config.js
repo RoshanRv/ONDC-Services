23df5d4e043e8853086e11f9bdd5a63ad301bad9
Roshan <nkroshankumar@gmail.com>
2022-08-27 08:40:08 +0530

    cors enabled

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+  async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://api.example.com/:path*',
+        },
+      ]
+    },
+};
