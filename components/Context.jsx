a85dfce41531271c31aaebf00d094738b52fd0db
Roshan <nkroshankumar@gmail.com>
2022-09-28 15:32:44 +0530

    userCoords added

--- components/Context.jsx
+++ components/Context.jsx
@@ -1,17 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from "react"
+import { getCurrentLocation } from "../util/util"
 
-export const Context = createContext('')
+export const Context = createContext("")
 
-const Store = ({children})=>{
+const Store = ({ children }) => {
+    const [userData, setUserData] = useState({})
+    const [userCoords, setUserCoords] = useState({})
 
-    const [userData,setUserData]=useState({})
-
-    return(
-        <Context.Provider value={[userData,setUserData]} >
+    return (
+        <Context.Provider
+            value={{ userData, setUserData, userCoords, setUserCoords }}
+        >
             {children}
         </Context.Provider>
     )
-
 }
 
 export default Store
